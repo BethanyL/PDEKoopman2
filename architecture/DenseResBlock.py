399634e59fc2e727198c66a35979b56efe9abbe9
Bethany Lusch <blusch@anl.gov>
2021-09-30 21:56:03 -0500

    add flexibility to DenseResBlock to have hidden layers with different width than input layer's width

--- architecture/DenseResBlock.py
+++ architecture/DenseResBlock.py
@@ -8,6 +8,7 @@ class DenseResBlock(keras.layers.Layer):
     def __init__(self,
                  n_inputs=128,
                  num_hidden=4,
+                 hidden_width='default',
                  hidden_config=dict(activation='relu',
                                     kernel_initializer='he_normal'),
                  output_config=dict(activation=None,
@@ -19,8 +20,9 @@ class DenseResBlock(keras.layers.Layer):
         Arguments:
             n_inputs -- the number of inputs to the network
                 (spatial discretization of the PDE)
-            num_hidden -- the number of hidden layers. Each will have the
-                same width as the input layer
+            num_hidden -- the number of hidden layers. 
+            hidden_width -- width of each hidden layer. By default, this is
+                the same width as the input layer. 
             hidden_config -- Python dictionary with keyword arguments to
                 pass to the dense hidden layers
             output_config -- Python dictionary with keyword arguments to
@@ -33,8 +35,12 @@ class DenseResBlock(keras.layers.Layer):
         self.n_inputs = n_inputs
         self.num_hidden = num_hidden
 
+        if hidden_width == 'default':
+            hidden_width = n_inputs
+        self.hidden_width = hidden_width 
+
         # Construct a list of the hidden layers used in this block
-        self.layers = [keras.layers.Dense(n_inputs,
+        self.layers = [keras.layers.Dense(hidden_width,
                                           name='hidden{}'.format(i),
                                           **hidden_config)
                        for i in range(num_hidden)]
